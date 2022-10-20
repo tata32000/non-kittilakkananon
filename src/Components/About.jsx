fb15ac0195f62a812b625fc7e4a951c97f0b0ba8
Non Kittilakkananon <tata32000@hotmail.com>
2022-10-20 10:38:48 +0800

    added info

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,20 +3,28 @@ export default function About() {
   return (
     <>
       <div id="about">
-        <h1>About me</h1>
+        <h1>Who am I?</h1>
         <p>
           Second-year Electrical Engineering student at the National University
           of Singapore, passionate about Software Engineering.
         </p>
-        <p>
-          I have experience building a web application with my project partner
-          using React and Firebase under Independent Software Development
-          Project (Orbital).{" "}
+        <h2>Education</h2>
+        <p id="info">
+          National University of Singapore <br />
+          <em>Bachelor of Engineering - BE, Electrical Engineering </em>
+          <br />
+          <em>Aug 2021 - May 2025</em>
         </p>
-        <h2>Projects:</h2>
+        <p id="info">
+          Auckland Grammar School <br />
+          <em>CAIE A-Level </em>
+          <br />
+          <em>Jan 2017 - Dec 2020</em>
+        </p>
+        <h2>Projects</h2>
         <h3>
           <a
-            href="http://creativenoters.vercel.app/"
+            href="https://github.com/parnikkapore/quiknotes-fe"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -26,22 +34,32 @@ export default function About() {
         <ul>
           <li>
             Developed a web application using React and Firebase to allow users
-            to annotate on images and pdf.
+            to annotate on images and pdf
           </li>
           <li>
-            <a
-              href="https://github.com/parnikkapore/quiknotes-fe"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github repo
-            </a>
+            NUS Orbital (Apollo 11) LEVEL OF ACHIEVEMENT FOR THE ORBITAL
+            PROGRAMME
           </li>
+          <li>Technologies: React.js, Firebase, MUI, Konva.js, pdf.js</li>
         </ul>
-        <h2>Skills:</h2>
+        <h3>
+        <a
+            href="https://github.com/tata32000/non-kittilakkananon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Personal Website
+          </a>
+        </h3>
+        <ul>
+          <li>My personal website to showcase my projects and resume</li>
+          <li>Technologies: React.js, React Bootstrap</li>
+        </ul>
+        <h2>Certificates</h2>
+        <h2>Skills</h2>
         <ul>
           <li>Programming Languages: Python, Java, JavaScript, C, C++</li>
-          <li>Web Development: React.js, Firebase, HTML, CSS</li>
+          <li>Technologies: React.js, Git, Firebase, Vim, Arduino, HTML, CSS</li>
         </ul>
       </div>
     </>
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
     <>
-      <h1>Contact me</h1>
+      <h1>Contact</h1>
       <p>Email: <a href = "mailto: tata32000@hotmail.com">tata32000@hotmail.com</a></p>
     </>
   );
