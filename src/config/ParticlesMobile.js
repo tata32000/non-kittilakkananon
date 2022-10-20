cf10acafbef84c2eb7b44eac73506ea4f50301fb
Non Kittilakkananon <tata32000@hotmail.com>
2022-10-20 13:48:57 +0800

    responsive tsparticle effect

---
+++ src/config/ParticlesMobile.js
@@ -0,0 +1,17 @@
+const ParticlesMobile = {
+    fpsLimit: 60,
+    particles: {
+        color: {
+            value: "#cccccc",
+        },
+        links: {
+            enable: true,
+            color: "#cccccc",
+            distance: 50,
+        },
+        move: {
+            enable: true,
+        },
+    },
+};
+export default ParticlesMobile;
