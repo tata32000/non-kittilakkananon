8c20f8c9e2e703175f05e87d0a1799635e268415
Non Kittilakkananon <tata32000@hotmail.com>
2022-10-22 12:06:43 +0800

    add form functionality

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,58 @@
+import { useState } from "react";
+import { send } from "@emailjs/browser";
+
 export default function Contact() {
+  const [toSend, setToSend] = useState({
+    from_name: "",
+    to_name: "Non Kittilakkananon",
+    message: "",
+    reply_to: "",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send("service_15c8l47", "template_cs307xg", toSend, "4nMOFWJfLJDWb8TKG")
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+      })
+      .catch((err) => {
+        console.log("FAILED...", err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <h1>Contact</h1>
-      <p>Email: <a href = "mailto: tata32000@hotmail.com">tata32000@hotmail.com</a></p>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="from_name"
+          placeholder="from name"
+          value={toSend.from_name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="message"
+          placeholder="Your message"
+          value={toSend.message}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="reply_to"
+          placeholder="Your email"
+          value={toSend.reply_to}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <p>
+        Email: <a href="mailto: tata32000@hotmail.com">tata32000@hotmail.com</a>
+      </p>
     </>
   );
 }
